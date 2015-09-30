759e266c5bed2113e886192316fb62b203c913c7
Jim Cooper <jim-cooper@pluralsight.com>
2015-09-30 13:29:45 -0700

    Complete

---
+++ user/user-acceptance-spec.js
@@ -0,0 +1,35 @@
+var usersController = require('./users-controller')
+
+describe('users api controller', function() {
+  var cassandra = require('cassandra-driver')
+  var client = new cassandra.Client({ contactPoints: ['cassandra.local'], keyspace: 'double_loop_demo'});
+  var savedUser
+
+  beforeEach(function(done) {
+    savedUser = {
+      id: cassandra.types.Uuid.random(),
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+    client.execute('INSERT INTO users(id, last_name, first_name) values(:id, :lastName, :firstName)', savedUser, {prepare: true},  function(err, result) { done() })
+  })
+
+  afterEach(function(done) {
+    client.execute('DELETE FROM users WHERE id = ?', [savedUser.id], {prepare: true},  function(err, result) { done() })
+  })
+  describe('get user', function() {
+    var returnedUser
+    beforeEach(function(done) {
+       usersController.get(savedUser.id).then(function(user) { returnedUser = user; done() })
+    })
+    it('should map the user id', function() {
+      expect(returnedUser.id.equals(savedUser.id)).toBeTruthy()
+    })
+    it('should map the first name', function() {
+      expect(returnedUser.firstName).toEqual(savedUser.firstName)
+    })
+    it('should map the last name', function() {
+      expect(returnedUser.lastName).toEqual(savedUser.lastName)
+    })
+  })
+})
---
+++ user/users-controller-spec.js
@@ -0,0 +1,46 @@
+var usersController = require('./users-controller')
+var usersRepository = require('./users-repository')
+var mockRequire = require('mock-require')
+var sinon = require('sinon')
+var q = require('q')
+var mockUsersRepository
+
+describe('users api controller', function() {
+  it('should have a get function', function() {
+    expect(usersController.get).toBeTruthy();
+  })
+
+  describe('get', function() {
+    var result
+    var userFromRepository = {id: 'abc', first_name: 'Jane', last_name: 'Smith'}
+
+    beforeEach(function(done) {
+      var deferred = q.defer()
+      mockUsersRepository = sinon.stub(usersRepository)
+      mockUsersRepository.get.returns(deferred.promise)
+      mockRequire('./users-repository', mockUsersRepository)
+
+      usersController.get('123-abc').then(function(response) {
+        result = response
+        done()
+      })
+
+      deferred.resolve(userFromRepository)
+    })
+    afterEach(function() {
+      sinon.restore(usersRepository)
+    })
+    it('should get the user from the repository', function() {
+      expect(mockUsersRepository.get.calledWith('123-abc')).toBeTruthy()
+    })
+    it('should map the id', function() {
+      expect(result.id).toEqual(userFromRepository.id)
+    })
+    it('should map the firstName', function() {
+      expect(result.firstName).toEqual(userFromRepository.first_name)
+    })
+    it('should map the lastName', function() {
+      expect(result.lastName).toEqual(userFromRepository.last_name)
+    })
+  })
+})
